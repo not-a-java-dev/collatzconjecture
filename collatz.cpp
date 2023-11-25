cd315498f646a58d445e9559f6a92f17ff662a82
NAJD <eastroqq@gmail.com>
2023-11-25 10:45:10 -0300

    Documentation and some smol thingys (i didnt want to document but whatever)

--- collatz.cpp
+++ collatz.cpp
@@ -2,14 +2,16 @@
 void cstep(int *p);
 int main() {
    int n;
-   //im not going to document anything since not a single person will see this
+   //get input
    std::cout << "Input: ";
    std::cin >> n;
    if (std::cin.fail()) {
+      //nuh uh that aint good input please dont do that
       std::cout << "Please enter a number\n";
       return -1;
    }
    int *np = &n;
+   //you know the drill
    while (n > 1) {
       cstep(np);
       std::cout << n << "\n";
@@ -17,9 +19,12 @@ int main() {
    return 0;
 }
 void cstep(int *p) {
+   //if n is even divide by 2
    if (*p % 2 == 0) {
       *p /= 2;
+   //if not then multiply by 3 and add 1
    } else {
-      *p = *p * 3 + 1;
+      (*p) *= 3;
+      (*p)++ ;
    }
 }
